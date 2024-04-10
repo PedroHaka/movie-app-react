aec29828af267d71dd262d7aa1250b03777b1259
Pedro Haka <pedrohaka94@gmail.com>
2024-04-10 12:23:32 -0300

    Initial commit - Implement React folder structure

---
+++ movie-app-react/src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='screen'>
+        The App is Working as Expected!!!
+      </div>
+    </>
+  )
+}
+
+export default App
